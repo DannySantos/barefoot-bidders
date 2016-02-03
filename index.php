f5c723e76818b3c35cad5b82b9089b79df82c70c
DannySantos <dannyangelosanto@gmail.com>
2016-02-03 10:23:29 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<!Doctype html>
+<html>
+
+	<head>
+
+		<title>Barefoot Bidders</title>
+
+		<link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+	</head>
+
+	<body>
+
+		<header>
+
+			<h1>
+				<a href="">
+					<img src="images/barefoot-bidders-logo.jpg" alt="Barefoot Bidders" width="100%">
+				</a>
+			</h1>
+
+			<a href="">
+				<img src="images/navicon.png" alt="Navigation Button">
+			</a>
+
+			<p class="clear"></p>
+
+			<nav>
+
+				<ul>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+
+			</nav>
+
+		</header>
+
+		<div class="jumbotron">
+
+			<h2>Vote like a butterfly, bid like a bee</h2>
+
+			<a href="#" class="button">Start a campaign</a>
+
+		</div>
+
+		<div>
+
+			<h2>How it works</h2>
+
+			<p class="instructions bottom-left">you ask a question...</p>
+
+			<p class="clear"></p>
+
+			<h3>Should Sallyella go to the Melbourne Races dressed as a donkey?</h3>
+
+			<img src="images/campaign-image-should-sallyella-go-to-the-melbourne-races-dressed-as-a-donkey.jpg" class="content-image" alt="Campaign image: Should Sallyella go to the Melbourne Races dressed as a donkey?">
+
+			<div class="options">
+
+				<p>Yes</p>
+				<p>$2000</p>
+				<span>Add Your Bid</span>
+
+			</div>
+
+			<div class="options">
+
+				<p>No</p>
+				<p>$2000</p>
+				<span>Add Your Bid</span>
+
+			</div>
+
+			<p class="clear"></p>
+
+			<p class="instructions">...your friends and family bid on the answers...</p>
+
+			<img src="images/winning-option-image.png" class="content-image" alt="Winning option image: Sallyella is dressed as a donkey!">
+
+			<p class="instructions top-left">...whichever option raises the most money wins...</p>
+
+			<p class="instructions bottom-right">...and a charity of your choice receives the donations!</p>
+
+			<img src="images/charity-thumbnail-buk-bilong-pikinini.jpg" class="content-image" alt="Charity image: Buk Bilong Pikinini">
+
+		</div>
+
+		<div class="separator-bg-2">
+
+			<span>$9000</span>
+			<p>Raised so far!</p>
+			<p>from 43 bidders</p>
+
+			<a href="start-a-campaign" class="button">Start a Campaign</a>
+
+		</div>
+
+		<div>
+
+			<div class="campaign-container">
+
+				<h3>Should Sallyella go the fro?</h3>
+
+				<img src="images/campaign-image-should-sallyella-go-the-fro.png" class="content-image" alt="Campaign image: should Sallyella go the fro?">
+
+				<div class="options">
+
+					<p>Option 1</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<div class="options">
+
+					<p>Option 2</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<p class="clear"></p>
+
+				<a href="campaign-page-should-sallyella-go-the-fro">Go to campaign</a>
+
+			</div>
+
+			<div class="campaign-container">
+
+				<h3>Should Sallyella go the fro?</h3>
+
+				<img src="images/campaign-image-should-sallyella-go-the-fro.png" class="content-image" alt="Campaign image: should Sallyella go the fro?">
+
+				<div class="options">
+
+					<p>Option 1</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<div class="options">
+
+					<p>Option 2</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<p class="clear"></p>
+
+				<a href="campaign-page-should-sallyella-go-the-fro">Go to campaign</a>
+
+			</div>
+
+			<div class="campaign-container">
+
+				<h3>Should Sallyella go the fro?</h3>
+
+				<img src="images/campaign-image-should-sallyella-go-the-fro.png" class="content-image" alt="Campaign image: should Sallyella go the fro?">
+
+				<div class="options">
+
+					<p>Option 1</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<div class="options">
+
+					<p>Option 2</p>
+					<p>$2000</p>
+					<span>Add Your Bid</span>
+
+				</div>
+
+				<p class="clear"></p>
+
+				<a href="campaign-page-should-sallyella-go-the-fro">Go to campaign</a>
+
+			</div>
+
+		</div>
+
+		<div class="separator-bg-1">
+
+			<h2>Need some inspiration?</h2>
+
+			<a href="campaign-deas" class="button">Browse campaign ideas</a>
+
+		</div>
+
+		<footer>
+			
+			<ul>
+				<li><a href="">About us</a></li>
+				<li><a href="">Who we are</a></li>
+				<li><a href="">Guide to creating a campaign</a></li>
+				<li><a href="">Campaign rules</a></li>
+			</ul>
+			
+			<ul>
+				<li><a href="">Contact</a></li>
+				<li><a href="">FAQs</a></li>
+				<li><a href="">Support</a></li>
+				<li><a href="">T&amp;Cs</a></li>
+				<li><a href="">Privacy policy</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Sign up</a></li>
+			</ul>
+
+			<p class="clear"></p>		
+
+			<hr>
+			
+			<img src="images/barefoot-bidders-logo.png" alt="Barefoot Bidders Logo">	
+
+		</footer>
+
+	</body>
+
+</html>
